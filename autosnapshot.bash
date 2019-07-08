44dd42e28da5b15bc70cc2d8d37beb86cd409041
Tobias Alroe <tobiasalroe@gmail.com>
2019-07-08 12:12:16 +0200

    Now parses argumets
    Easier to implement using eg. cron.
    You can make cron entries if you would like to have different retention policies

--- autosnapshot.bash
+++ autosnapshot.bash
@@ -1,4 +1,41 @@
 #!/bin/bash
+set -xe
+showHelp(){
+  echo "Usage: autosnapshot OPTION VM_ID..."
+  echo "Automatic snapshotting for Proxmox PVE"
+  echo
+  echo "  -p, --purge-date      Removes automatic snapshots when whey are older than this."
+  echo "                        Uses `date --date` formatting. Eg. '10 days ago'" 
+  exit 0 
+}
+
+if [[ $# -eq 0 ]] ; then
+  showHelp
+fi
+
+# argument parsing sample: https://stackoverflow.com/a/14203146/1481004
+VMS=()
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -p|--purge-date)
+    PURGE_DATE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -h|--help)
+    showHelp
+    ;;
+    *)    # unknown option
+    VMS+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+
 getSnapshots(){
         qm listsnapshot $1 | grep -o -e 'auto_[0-9]*' | sort | uniq
 }
@@ -9,12 +46,11 @@ takeSnapshot(){
 }
 
 removeOldSnapshots(){
-        DAYS_AGO=$(date +%s --date="10 days ago")
+        DAYS_AGO=$(date +%s --date="${PURGE_DATE}")
         getSnapshots $vm  | cut -c 6- | xargs -L1 -I {} sh -c "\
                 [ $DAYS_AGO -gt {} ] && qm delsnapshot  $vm auto_{}"
 }
 
-VMS=( 102 103 )
 for vm in "${VMS[@]}"
 do
         takeSnapshot $vm
